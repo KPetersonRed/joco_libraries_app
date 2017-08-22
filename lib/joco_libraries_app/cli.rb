519a4c12ebb12e55db9ed962fc558631cb8eae49
Kristy Peterson <kpeterson.kap@gmail.com>
2017-08-22 05:33:52 +0000

    added files to be tracked by git

---
+++ lib/joco_libraries_app/cli.rb
@@ -0,0 +1,35 @@
+#CLI Controller (responsible for user interface)
+
+class JocoLibrariesApp::CLI
+
+  def call #beginning of interface
+    puts "Thank you for using the Kansas Public Libraries database to find more information on public libraries nearest to you."
+    get_zipcode
+  end
+
+  def get_zipcode
+    puts "Please enter your 5 digit zipcode.  We will return information on the 5 libraries closest to you within a 25 mile radius."
+    zip = gets.chomp
+    valid_input?(zip)
+    zip_lookup(zip)
+  end
+
+  def valid_input?(zip)
+     #all numbers?  5 characters in length?
+  end
+
+  def zip_lookup(zip)
+    # scrape www.publiclibraries.com/kansas.htm
+    # Found?
+      local_libraries_menu #within 25 mile radius
+    #not found?
+      #msg to user, reenter zip or try a different one
+  end
+
+  def local_libraries_menu
+    puts "Please enter the number of the library you'd like more info."
+    input = gets.chomp
+  end
+
+
+end
