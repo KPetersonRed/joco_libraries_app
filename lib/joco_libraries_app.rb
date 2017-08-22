3a0d776d20a15072756e4a920a02a7a281f9e69e
Kristy Peterson <kpeterson.kap@gmail.com>
2017-08-22 04:45:48 +0000

    set-up running shell ofbin file and cli class.

--- lib/joco_libraries_app.rb
+++ lib/joco_libraries_app.rb
@@ -1,5 +1,3 @@
-require "joco_libraries_app/version"
-
-module JocoLibrariesApp
-  puts "hello world"
-end
+#This file acts as my environment. Single file to require which will then require all other dependencies.
+require_relative "joco_libraries_app/version"
+require_relative './joco_libraries_app/cli'
