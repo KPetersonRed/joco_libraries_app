fc8de111fb70133616717477b5e7131e56f77d05
Kristy <kpeterson.kap@gmail.com>
2017-09-06 17:37:55 -0500

    Create scraper.rb

---
+++ lib/joco_libraries_app/scraper.rb
@@ -0,0 +1,19 @@
+class JocoLibrariesApp::Scraper
+
+  def get_page
+    doc = Nokogiri::HTML(open("https://www.jocolibrary.org/locations"))
+    #binding.pry
+  end
+
+  def scrape_libraries_index
+    self.get_page.css(".location-branch")
+  end
+
+  def make_libraries
+    scrape_libraries_index.each {|x|
+      JocoLibrariesApp::Libraries.new_from_index_page(x)
+    }
+  end
+
+
+end
